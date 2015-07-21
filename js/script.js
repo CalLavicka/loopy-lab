be71ce949c562d074627544d6fc34be2f2123cc4
tejak <sk2759@cornell.edu>
2015-07-21 12:29:27 -0400

    initial lab commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+                  
+
+// part1: write a function that countsdown
+                  
+   
+                  
+// part2: write a function that calculates factorial
+                  
+                  
+                  
+// part3: write a function that calcuales the ____ fibonacci number of the sequence                
+                  
+                  
+
+                  
+});
